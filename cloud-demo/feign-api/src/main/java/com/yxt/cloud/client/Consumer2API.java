22808ea8d4c528dfededae4404f1b51b82c0b6aa
qincasin <qincasin@163.com>
2022-10-17 22:43:58 +0800

    feat: ribbon的重试日志查找

--- cloud-demo/feign-api/src/main/java/com/yxt/cloud/client/Consumer2API.java
+++ cloud-demo/feign-api/src/main/java/com/yxt/cloud/client/Consumer2API.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("consumer2")
+@FeignClient("consumer-2")
 @RequestMapping("/test")
 public interface Consumer2API {
     @GetMapping("/getConsumer")
